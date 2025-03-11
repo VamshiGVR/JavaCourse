f1368fd6b55ed5b56a3dd20dc631d7dc3d9a6ccb
Vamshimittu1 <gunvamshidharreddy@gmail.com>
2025-03-11 12:55:15 -0400

    added & reordered files

---
+++ Projects/AvgCalAge/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        int Ages[] = {25,29,40,21,55};
+        int sum = 0;
+
+        // Age at particular place
+        System.out.println("Ages: " + Ages[0]);
+        for(int age:Ages){
+            sum +=age;
+        }
+        // Totat of all ages
+        System.out.println("Total: " + sum);
+
+        // Average of all ages.
+        int avg = sum/Ages.length;
+        System.out.println("Average: " + avg);
+
+        // Find lowest Age
+        int lowestAge= Ages[0];
+        for(int age:Ages){
+            if (lowestAge>age)
+            lowestAge = age;
+        }
+        System.out.println("Lowest Age is " + lowestAge);   
+        
+        // Find higest age
+        int higestAge = Ages[0];
+        for(int age:Ages){
+            if(higestAge<age)
+            higestAge = age;
+        }
+        System.out.println("highest Age is " + higestAge);   
+
+    }
+}
---
+++ Projects/AvgCalAge/Readme.md
@@ -0,0 +1,4 @@
+# Program
+
+1) That calculates the average of different ages.
+2) Finds the lowest age among different ages..
